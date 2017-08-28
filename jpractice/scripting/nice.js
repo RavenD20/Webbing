407dde20d15e7ab5b130b933592ec8140c7945cf
Ravend20 <Fleetsd@yahoo.com>
2017-08-28 16:25:18 -0400

    wubbalubaadubdub

--- jpractice/scripting/nice.js
+++ jpractice/scripting/nice.js
@@ -7,3 +7,12 @@ function change() {
 function change2() {
   document.images[0].src="pizzapics/pizza.gif"
 }
+
+function changeStatus(){
+  var curMessage ="good";
+  function changeMessage(){
+    if (curMessage == "good2"){
+      document.images[0].src="pizzapics/pizza.gif"
+    }
+  }
+}
