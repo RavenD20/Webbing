d01ef47daf0280df0428ad88cabaaaab246c7455
Ravend20 <Fleetsd@yahoo.com>
2017-08-30 11:28:59 -0400

    ffff

---
+++ jpractice/scripting/space.js
@@ -0,0 +1,10 @@
+
+
+function mercury(){
+
+  var ford = {name:"f150" , year:2005, model:"XLT"};
+
+// document.getElementById('display').innerHTML = ford.name;
+document.getElementById('display').innerHTML = ford.year;
+
+}
